7fdd496422f19e73455d62dd36a69a73d097fab9
PritamWakadkar <pritamwakadkar@gmail.com>
2026-01-16 23:15:04 +0530

    commit

--- src/pages/Fifthhpart.jsx
+++ src/pages/Fifthhpart.jsx
@@ -1,74 +1,40 @@
-import React from 'react'
+ import { useGSAP } from '@gsap/react'
+import React, { useRef } from 'react'
+ 
+ const Fifthhpart = () => {
+ const divref = useRef()
+ 
+   useGSAP(()=>{
 
-const fifthpart = () => {
-  return (
-   <div className="h-screen w-full bg-black">
-  <h1 className="font-extrabold text-4xl text-white px-20 py-10">
-    Ride
-  </h1>
+   })
 
-  <div className="flex items-center justify-evenly w-full px-10 gap-5">
 
-    {/* Left Card */}
-    <div className="relative h-[400px] w-[400px]">
-      <img
-        className="h-full w-full object-cover"
-        src="https://www.royalenfield.com/content/dam/royal-enfield-revamp/home/new/irides/webp/rides-new.webp"
-        alt=""
-      />
-      <h1 className="absolute bottom-4 left-4 text-white text-xl font-bold">
-        Rides
-      </h1>
+   return (
+   <div className='h-screen w-full bg-black text-white '>
+       <h1 className=' font-extrabold text-5xl  px-20 py-10'>RIde</h1>
+   <div className='flex items-center content-center  px-50 gap-10'>
+      <div>
+  <img src="https://www.royalenfield.com/content/dam/royal-enfield-revamp/home/new/irides/webp/rides-new.webp" alt="" />
+ </div>
+   <div className=''>
+    <div className='pb-4'>
+      <img src="https://www.royalenfield.com/content/dam/royal-enfield-revamp/home/new/irides/webp/rentals-ride-home.webp" alt="" />
     </div>
-
-    {/* Middle Column */}
-    <div className="flex flex-col gap-6">
-
-      <div className="relative h-[200px] w-[350px]">
-        <img
-          className="h-full w-full object-cover"
-          src="https://www.royalenfield.com/content/dam/royal-enfield-revamp/home/new/irides/webp/rentals-ride-home.webp"
-          alt=""
-        />
-        <h1 className="absolute bottom-2 left-3 text-white font-semibold">
-          Rentals
-        </h1>
-      </div>
-
-      <div className="relative h-[200px] w-[350px]">
-        <img
-          className="h-full w-full object-cover"
-          src="https://www.royalenfield.com/content/dam/royal-enfield-revamp/home/new/irides/webp/tours-desktop.webp"
-          alt=""
-        />
-        <h1 className="absolute bottom-2 left-3 text-white font-semibold">
-          Tours
-        </h1>
-      </div>
-
+    <div>
+      <img src="https://www.royalenfield.com/content/dam/royal-enfield-revamp/home/new/irides/webp/tours-desktop.webp" alt="" />
     </div>
-
-    {/* Right Card */}
-    <div className="relative h-[400px] w-[400px]">
-      <img
-        className="h-full w-full object-cover"
-        src="https://www.royalenfield.com/content/dam/royal-enfield/india/rides/events/motoverse-2025/motoverse-ride.webp"
-        alt=""
-      />
-      <h1 className="absolute bottom-4 left-4 text-white text-xl font-bold">
-        Motoverse
-      </h1>
-    </div>
-
-  </div>
+   </div>
+ <div>
+  <img src="https://www.royalenfield.com/content/dam/royal-enfield/india/rides/events/motoverse-2025/motoverse-ride.webp" alt="" />
+ </div>
 
 
-  <div>
-    <h1 className='text-white font-bold text-4xl px-20 py-20'>Latest</h1>
-  </div>
-</div>
+   </div>
 
-  )
-}
 
-export default fifthpart
+
+     </div>
+   )
+ }
+ 
+ export default Fifthhpart
